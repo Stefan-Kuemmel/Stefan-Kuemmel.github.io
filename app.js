14d8b1e66e1f497cc8f3ff370a1067728b06f973
Szabolcs Grünwald <sgruenwald@eurofunk.com>
2015-06-13 12:13:36 +0200

    add basic ui-router navigation examples

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 angular.module('MyApp', ['ui.router'])
-
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('index', {
+                url: "/",
+                views: {
+                    "select": {template: ""},
+                    "content": {templateUrl: "partials/mohnblume.html"}
+                }
+            })
+            .state('blume/nach-name', {
+                url: "/blume/nach-name",
+                views: {
+                    "select": {templateUrl: "partials/name-selection.html"},
+                    "content": {templateUrl: "partials/mohnblume.html"}
+                }
+            })
+        ;
+        $urlRouterProvider.otherwise("");
+    })
 ;
